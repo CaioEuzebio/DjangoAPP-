033716c2b08d14fe38960e7d75250b8453347a4b
Caio Euzebio <caio.145@hotmail.com>
2019-12-12 14:21:27 -0300

    Add CPF Field

--- clientes/admin.py
+++ clientes/admin.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from .models import Clientes
 from .models import Telefone
+from .models import CPF
 
 
 admin.site.register(Clientes)
 admin.site.register(Telefone)
+admin.site.register(CPF)
 
 # Register your models here.
--- clientes/models.py
+++ clientes/models.py
@@ -2,6 +2,13 @@ from django.db import models
 
 # Create your models here.
 
+class CPF(models.Model):
+    numero = models.CharField(max_length=20)
+    data_exp = models.DateTimeField(auto_now=False)
+
+    def __str__(self):
+        return self.numero
+
 
 
 class Clientes(models.Model):
@@ -10,6 +17,7 @@ class Clientes(models.Model):
     salario = models.DecimalField(max_digits=10, decimal_places=2)
     idade = models.IntegerField()
     email = models.EmailField()
+    cpf = models.OneToOneField(CPF, blank=True, null=True, on_delete=models.PROTECT)
 
     def __str__(self):
         return self.nome
